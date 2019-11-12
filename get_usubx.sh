4fd1147b18e010e21bfbb74d3befea0758e94130
ellenfranceszakreski <efzakreski@mac.com>
2019-11-12 14:46:50 -0500

    Update get_usubx.sh

--- get_usubx.sh
+++ get_usubx.sh
@@ -6,6 +6,6 @@ AnalysisDir=/data/scratch/zakell/fmri_nov2019 #<-make sure this is correct
 #
 for subx in `cat $AnalysisDir/Scripts/subjects.txt`
 do
-  cp $SourceDir/Input/$subx/usub*.nii $AnalysisDir/Input/$subx/usub*.nii
+  cp $SourceDir/Input/$subx/usub*.nii $AnalysisDir/Input/$subx
 done
 ### DONE
