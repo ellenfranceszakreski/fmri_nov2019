eed442168d4726aa7f747ef8b15030477932a325
ellenfranceszakreski <efzakreski@mac.com>
2019-11-12 14:44:09 -0500

    Create get_usubx.sh

---
+++ get_usubx.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+SourceDir=/data/scratch/zakell/fmri_oct2019
+AnalysisDir=/data/scratch/zakell/fmri_nov2019 #<-make sure this is correct
+
+#
+for subx in `cat $AnalysisDir/Scripts/subjects.txt`
+do
+
+done
