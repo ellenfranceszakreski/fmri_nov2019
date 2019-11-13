870b908bd2aaba2b9bbca348d3d3852ae113b7f4
ellenfranceszakreski <efzakreski@mac.com>
2019-11-13 07:16:38 -0500

    Update backup_realign.sh

--- backup_realign.sh
+++ backup_realign.sh
@@ -4,7 +4,7 @@
 AnalysisDir=/data/scratch/zakell/fmri_nov2019
 for subx in `cat $AnalysisDir/Scripts/subjects.txt`; do
   subxDir=$AnalysisDir/Input/$subx
-  if [ -f $subxDir/realign_done.txt ] && [ ! -d $subxDir/realign ]; then
+  if [ -f $subxDir/realign_done.mat ] && [ ! -d $subxDir/realign ]; then
     mkdir $subxDir/realign
     cp -v $subxDir/mean*.nii $subxDir/realign
     cp -v $subxDir/usub*run*.nii $subxDir/realign
