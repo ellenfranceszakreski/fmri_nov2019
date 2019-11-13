d95c0970fa0474730430d04c5e92ef08d4443d3c
ellenfranceszakreski <efzakreski@mac.com>
2019-11-13 05:29:39 -0500

    Create ave_job_template.m

---
+++ ave_job_template.m
@@ -0,0 +1,22 @@
+% append code after defining variable subx
+% e.g. subx='sub2'
+
+%% set up cluster
+number_of_cores=12;
+d=tempname();% get temporary directory location
+mkdir(d);
+% create cluster
+cluster = parallel.cluster.Local('JobStorageLocation',d,'NumWorkers',number_of_cores);
+matlabpool(cluster, number_of_cores);
+
+%% run analysis
+addpath(genpath(fullfile(spm('dir'),'config')));
+AnalysisDir='/data/scratch/zakell/fmri_nov2019';
+subxDir=fullfile(AnalysisDir,'Input',subx);
+jobs = {fullfile(AnalysisDir,'Scripts','ave_job.m')};
+inputs{1,1} = {subxDir};
+spm('defaults', 'FMRI');
+spm_jobman('run', jobs, inputs{:});
+% save file to indicate completion
+save(fullfile(subxDir,'ave_done.mat'), 'jobs','-mat');
+% done
