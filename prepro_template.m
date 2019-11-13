706e4b442a197821681aa04bed7281ef2b9941aa
ellenfranceszakreski <efzakreski@mac.com>
2019-11-12 21:16:28 -0500

    Update prepro_template.m

--- prepro_template.m
+++ prepro_template.m
@@ -1,9 +1,21 @@
 % append code after defining variable subx
 % e.g. subx='sub2'
+
+%% set up cluster
+number_of_cores=12;
+d=tempname();% get temporary directory location
+mkdir(d);
+% create cluster
+cluster = parallel.cluster.Local('JobStorageLocation',d,'NumWorkers',number_of_cores);
+matlabpool(cluster, number_of_cores);
+
+%% run analysis
 addpath(genpath(fullfile(spm('dir'),'config')));
 AnalysisDir='/data/scratch/zakell/fmri_nov2019';
 jobs = {fullfile(AnalysisDir,'prepro_job.m')};
 inputs{1,1} = {fullfile(AnalysisDir,'Input',subx)};
 spm('defaults', 'FMRI');
 spm_jobman('run', jobs, inputs{:});
+% save file to indicate completion
+save(fullfile(subxDir,'prepro_done.mat'), 'jobs','-mat');
 % done
