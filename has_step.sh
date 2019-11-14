42995d61d46f05c9c3331984af256ac0493715a3
ellenfranceszakreski <efzakreski@mac.com>
2019-11-14 13:59:29 -0500

    Update has_step.sh

--- has_step.sh
+++ has_step.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
-# has_step.sh <step name>
+# has_step.sh <step name> <optional subject list>
 # make list of subjects missing a step
 # step completion is indicated by existance of <step name>_done.mat file in subject's input folder.
 
 AnalysisDir=/data/scratch/zakell/fmri_nov2019 # <--make sure this is correct!
 # check inputs
-if [ $# -ne 1 ]; then
-	printf "ERROR: Incorrect number of inputs\nUsage: has_step.sh <step_name>\n" >&2
+if [ $# -eq 1 ]
+	SubjectList=$AnalysisDir/Scripts/subjects.txt
+elif [ $# -eq 2 ]
+	SubjectList="$2"
+else
+	printf "ERROR: Incorrect number of inputs\nUsage: has_step.sh <step_name> <optional subject list file>\n" >&2
 	exit 1
 fi
 ## validate step name
@@ -25,6 +29,16 @@ if $InvalidStepName; then
 	exit 1
 fi
 unset InvalidStepName ValidStepNames
+## validate subject list
+if [ ${#SubjectList} -eq 0 ]; then
+	echo "ERROR: Subject list cannot be empty." >&2
+	exit 1
+elif [ -f "$SubjectList" ]; then
+	echo "Subject list = "$SubjectList
+else
+	echo "ERROR: Cannot find subject list at: "$SubjectList >&2
+	exit 1
+fi
 ## list subjects missing this step
 
 MissingFile=$AnalysisDir/$StepName"_missing.txt"
@@ -33,7 +47,7 @@ test -f $MissingFile && rm $MissingFile
 touch $MissingFile
 printf "looking for %s_done.mat in each subject's input directory\n" $StepName 
 
-for subx in `cat $AnalysisDir/Scripts/subjects.txt`; do
+for subx in `cat "$SubjectList"`; do
 	ThisFile=$AnalysisDir/Input/$subx/$StepName"_done.mat"
 	test ! -f $ThisFile && echo $subx >> $MissingFile
 done
